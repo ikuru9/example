8fbcaab3c85c1db31baef9ce09bec1e690d59813
ikuru9 <ikuru9@gmail.com>
2024-01-04 20:34:28 +0900

    docs: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,10 @@
 module.exports = {
-  ignores: [(commit) => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'wip'],
+    ],
+  },
 }
